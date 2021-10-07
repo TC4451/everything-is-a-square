2949fd99c7fa2449251ca0c30bbffe5dcc06495e
Zilin Dai <zdai2@wpi.edu>
2021-10-07 19:44:01 -0400

    window can be closed

--- game.py
+++ game.py
@@ -5,6 +5,15 @@ gameWidth = 1600
 
 pygame.init()
 gameDisplay = pygame.display.set_mode((gameWidth, gameHeight))
-pygame.display.set_caption('Everything is a square')
+pygame.display.set_caption('Everything-is-a-square')
 clock = pygame.time.Clock()
 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+            
+    pygame.display.update()
+    clock.tick(60)
+    
