b39012ac77218676145e3dd31ef50825598b0fc0
Zilin Dai <zdai2@wpi.edu>
2021-10-07 19:55:38 -0400

    added square function

--- game.py
+++ game.py
@@ -1,19 +1,32 @@
 import pygame
 
-gameHeight = 900
-gameWidth = 1600
+game_height = 900
+game_width = 1600
+
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 200, 0)
+blue = pygame.Color(0, 0, 255)
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((gameWidth, gameHeight))
+game_display = pygame.display.set_mode((game_width, game_height))
 pygame.display.set_caption('Everything-is-a-square')
 clock = pygame.time.Clock()
+
+def draw_square(color, x, y, size):
+        pygame.draw.rect(game_display, color, [x, y, size, size])
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-            
+
+    draw_square(red, 20, 20, 30)
+
     pygame.display.update()
     clock.tick(60)
     
