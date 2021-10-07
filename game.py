9836750059c7ca143ed21778463eeb4ce8a6b9d9
Zilin Dai <zdai2@wpi.edu>
2021-10-07 19:35:26 -0400

    created game script, window appears

---
+++ game.py
@@ -0,0 +1,10 @@
+import pygame
+
+gameHeight = 900
+gameWidth = 1600
+
+pygame.init()
+gameDisplay = pygame.display.set_mode((gameWidth, gameHeight))
+pygame.display.set_caption('Everything is a square')
+clock = pygame.time.Clock()
+
